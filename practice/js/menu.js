c51f8ec6bb7ee1d15c6b612a7ae9f0955d35fb57
panzheng <apan0920@126.com>
2017-11-27 18:29:03 +0800

    2017-11-27
    2017-11-27

---
+++ practice/js/menu.js
@@ -0,0 +1,8 @@
+function showmenu(str){  
+        var subdiv=document.getElementById(str);  
+        subdiv.style.display="block";  
+}  
+function hidemenu(str){  
+        var subdiv=document.getElementById(str);  
+        subdiv.style.display="none";  
+}  
---
+++ practice/js/news.js
@@ -0,0 +1,3 @@
+/*$("#nav ul li").on(click,function(){
+	$(this).attr("background-color","red");
+});*/
